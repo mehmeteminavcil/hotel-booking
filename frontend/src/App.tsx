6c370c1c2ddeee3f23e6e72c972517e916f43b4b
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-02-20 23:58:12 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+
+function App() {
+
+
+  return (
+   <Router>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
+   </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,17 @@
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+
+const Layout = () => {
+  return (
+    
+    <div className="flex flex-col min-h-screen">
+        <Header/>
+        <Hero/>
+        oyeahh
+        <Footer/>
+    </div>
+  )
+}
+
+export default Layout
